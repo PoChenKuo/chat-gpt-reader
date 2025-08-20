5dccd3e69ebea518b143d446c163b20a75f187ba
PoChen <icerkure@gmail.com>
2025-08-21 05:19:50 +0800

    [feat] add hook

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/chat-gpt-reader/',
   plugins: [
     vue(),
     vueJsx(),
